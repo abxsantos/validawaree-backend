6fbfad4ca98f83652613eaed03a0d5404482d02e
Alexandre Xavier <ale.bxsantos@gmail.com>
2020-07-03 02:28:00 -0300

    Removed obsolete constructor code and tests and improved the documentation

--- tests/integration/test_validators/test_linearity_validator.py
+++ tests/integration/test_validators/test_linearity_validator.py
@@ -1,15 +1,15 @@
-import pytest
-
 from analytical_validation.validators.linearity_validator import LinearityValidator
 
 
 class TestLinearityValidator(object):
     def test_linearity_validator_integration_with_methods(self):
-        analytical_data = [[0.188, 0.192, 0.203], [0.349, 0.346, 0.348], [0.489, 0.482, 0.492], [0.637, 0.641, 0.641], [0.762,
-                           0.768, 0.786], [0.931, 0.924, 0.925]]
+        analytical_data = [[0.188, 0.192, 0.203], [0.349, 0.346, 0.348], [0.489, 0.482, 0.492], [0.637, 0.641, 0.641],
+                           [0.762,
+                            0.768, 0.786], [0.931, 0.924, 0.925]]
         concentration_data = [[0.008, 0.008016, 0.008128], [0.016, 0.016032, 0.016256], [0.02, 0.02004, 0.02032],
                               [0.027999996640000406, 0.028055996633280407, 0.02844799658624041], [0.032, 0.032064,
-                              0.032512], [0.04, 0.04008, 0.04064]]
+                                                                                                  0.032512],
+                              [0.04, 0.04008, 0.04064]]
         # Arrange
         linearity_validator = LinearityValidator(analytical_data, concentration_data)
         linearity_validator.ordinary_least_squares_linear_regression()
@@ -37,7 +37,7 @@ class TestLinearityValidator(object):
         assert linearity_validator.anova_f_value is not None
         assert linearity_validator.anova_f_pvalue is not None
         assert linearity_validator.is_normal_distribution
-        assert linearity_validator.is_homokedastic
+        assert linearity_validator.is_homoscedastic
         assert linearity_validator.durbin_watson_value is not None
 
     # def test_hplc_values(self):
