25650d4c93f04975ec843f2e5bc32b75632a50c1
Alexandre Xavier <ale.bxsantos@gmail.com>
2020-07-09 15:57:20 -0300

    Added CORS permission

--- src/analytical_validation/app.py
+++ src/analytical_validation/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 
 from analytical_validation.api import LinearityValidation
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 api.add_resource(LinearityValidation, '/linearity_result')
