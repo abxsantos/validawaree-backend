5532ad156685fa6a7ab761ace5c8f5c90c6b96a4
Alexandre Xavier <ale.bxsantos@gmail.com>
2020-07-11 05:15:18 -0300

    added flask test

--- src/analytical_validation/app.py
+++ src/analytical_validation/app.py
@@ -4,11 +4,17 @@ from flask_cors import CORS
 
 from analytical_validation.api import LinearityValidation
 
-app = Flask(__name__)
+
+def create_app():
+    app = Flask(__name__)
+    return app
+
+app = create_app()
+
 CORS(app)
 api = Api(app)
-
 api.add_resource(LinearityValidation, '/linearity_result')
+
 
 if __name__ == '__main__':
     app.run()
