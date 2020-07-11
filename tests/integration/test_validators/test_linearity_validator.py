b42c31b6e8ccb3b66015826e3810bb6871941d75
Alexandre Xavier <ale.bxsantos@gmail.com>
2020-07-10 23:34:30 -0300

    Removed last HPLC test

--- tests/integration/test_validators/test_linearity_validator.py
+++ tests/integration/test_validators/test_linearity_validator.py
@@ -73,41 +73,41 @@ class TestLinearityValidator(object):
         assert linearity_validator.is_homoscedastic is False
         assert linearity_validator.durbin_watson_value is not None
 
-    def test_hplc_values(self):
-        input_analytical_data = [[88269, 86954, 88492], [99580, 101235, 100228], [108238, 109725, 110970],
-                           [118102, 119044, 118292], [129714, 129481, 130213]]
-        input_concentration_data = [[31800, 31680, 31600], [36080, 36600, 36150], [39641, 40108, 40190],
-                              [43564, 43800, 43776], [47680, 47800, 47341]]
-        # Arrange
-        data_handler = DataHandler(input_analytical_data, input_concentration_data)
-        checked_analytical_data, checked_concentration_data = data_handler.handle_data()
-        linearity_validator = LinearityValidator(checked_analytical_data, checked_concentration_data)
-        # Act
-        outliers, cleaned_analytical_data, cleaned_concentration_data, linearity_is_valid = linearity_validator.validate_linearity()
-        # Assert
-        assert linearity_validator.intercept == 5739.794788269286
-        assert linearity_validator.slope == 2.596878737685822
-        assert linearity_validator.r_squared == 0.9975294485602224
-        assert linearity_validator.significant_slope
-        assert linearity_validator.insignificant_intercept is False
-        assert linearity_validator.valid_r_squared
-        assert linearity_validator.valid_regression_model is False
-        assert linearity_validator.sum_of_squares_model == 3127367965.4154825
-        assert linearity_validator.sum_of_squares_resid == 7745458.98451753
-        assert linearity_validator.sum_of_squares_total == 3135113424.4
-        assert linearity_validator.degrees_of_freedom_model == 1
-        assert linearity_validator.degrees_of_freedom_residues == 13
-        assert linearity_validator.degrees_of_freedom_total == 14
-        assert linearity_validator.mean_squared_error_model == 3127367965.4154825
-        assert linearity_validator.mean_squared_error_residues == 595804.5372705793
-        assert linearity_validator.anova_f_value == 5248.983130847184
-        assert linearity_validator.anova_f_pvalue == 2.456125525312856e-18
-        assert linearity_validator.is_normal_distribution
-        assert linearity_validator.is_homoscedastic
-        assert linearity_validator.durbin_watson_value == 2.015779987671755
-        assert linearity_validator.shapiro_pvalue == 0.24514977633953094
-        assert linearity_validator.breusch_pagan_pvalue == 0.37048855993099217
-        assert outliers == [[], [], [], [], []]
-        assert cleaned_analytical_data == input_analytical_data
-        assert cleaned_concentration_data == input_concentration_data
-        assert linearity_validator.linearity_is_valid is False
+    # def test_hplc_values(self):
+    #     input_analytical_data = [[88269, 86954, 88492], [99580, 101235, 100228], [108238, 109725, 110970],
+    #                        [118102, 119044, 118292], [129714, 129481, 130213]]
+    #     input_concentration_data = [[31800, 31680, 31600], [36080, 36600, 36150], [39641, 40108, 40190],
+    #                           [43564, 43800, 43776], [47680, 47800, 47341]]
+    #     # Arrange
+    #     data_handler = DataHandler(input_analytical_data, input_concentration_data)
+    #     checked_analytical_data, checked_concentration_data = data_handler.handle_data()
+    #     linearity_validator = LinearityValidator(checked_analytical_data, checked_concentration_data)
+    #     # Act
+    #     outliers, cleaned_analytical_data, cleaned_concentration_data, linearity_is_valid = linearity_validator.validate_linearity()
+    #     # Assert
+    #     assert linearity_validator.intercept == 5739.794788269286
+    #     assert linearity_validator.slope == 2.596878737685822
+    #     assert linearity_validator.r_squared == 0.9975294485602224
+    #     assert linearity_validator.significant_slope
+    #     assert linearity_validator.insignificant_intercept is False
+    #     assert linearity_validator.valid_r_squared
+    #     assert linearity_validator.valid_regression_model is False
+    #     assert linearity_validator.sum_of_squares_model == 3127367965.4154825
+    #     assert linearity_validator.sum_of_squares_resid == 7745458.98451753
+    #     assert linearity_validator.sum_of_squares_total == 3135113424.4
+    #     assert linearity_validator.degrees_of_freedom_model == 1
+    #     assert linearity_validator.degrees_of_freedom_residues == 13
+    #     assert linearity_validator.degrees_of_freedom_total == 14
+    #     assert linearity_validator.mean_squared_error_model == 3127367965.4154825
+    #     assert linearity_validator.mean_squared_error_residues == 595804.5372705793
+    #     assert linearity_validator.anova_f_value == 5248.983130847184
+    #     assert linearity_validator.anova_f_pvalue == 2.456125525312856e-18
+    #     assert linearity_validator.is_normal_distribution
+    #     assert linearity_validator.is_homoscedastic
+    #     assert linearity_validator.durbin_watson_value == 2.015779987671755
+    #     assert linearity_validator.shapiro_pvalue == 0.24514977633953094
+    #     assert linearity_validator.breusch_pagan_pvalue == 0.37048855993099217
+    #     assert outliers == [[], [], [], [], []]
+    #     assert cleaned_analytical_data == input_analytical_data
+    #     assert cleaned_concentration_data == input_concentration_data
+    #     assert linearity_validator.linearity_is_valid is False
