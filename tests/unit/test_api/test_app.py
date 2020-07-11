9b317c884a01bc9959dab46a41974b8620fe5596
Alexandre Xavier <ale.bxsantos@gmail.com>
2020-07-11 05:21:32 -0300

    Added flask restful tests

--- tests/unit/test_api/test_app.py
+++ tests/unit/test_api/test_app.py
@@ -1,30 +1,30 @@
 import json
 
 import pytest
-from flask import url_for
 
 url = 'http://127.0.0.1:5000'  # The root url of the flask app
 
-from analytical_validation.app import create_app
+from analytical_validation.app import app
 
 @pytest.fixture
 def client():
-    return create_app().test_client()
+    with app.test_client() as client:
+        yield client
 
 
+class TestApp(object):
+    def test_linearity_result_page_must_not_allow_get_in_linearity_result_page(self, client):
+        response = client.get('/linearity_result') # Assumses that it has a path of "/linearity_result"
+        assert response.status_code == 405  # Assumes that it will return a 405 response, method not allowed
 
-def test_linearity_result_page_must_not_allow_get_in_linearity_result_page(client):
-    r = client.get('http://127.0.0.1:5000/linearity_result') # Assumses that it has a path of "/linearity_result"
-    assert r.status_code == 405  # Assumes that it will return a 405 response, method not allowed
 
+    def test_linearity_result_page_must_assert(self, client):
+        mimetype = 'application/json'
+        headers = {
+            'Content-Type': mimetype,
+            'Accept': mimetype
+        }
+        data = {"analytical_data": "[[0.188, 0.192, 0.203], [0.349, 0.346, 0.348]]", "concentration_data": "[[0.008, 0.008, 0.008], [0.016, 0.016, 0.016]]"}
 
-def test_linearity_result_page_must_assert(client):
-    mimetype = 'application/json'
-    headers = {
-        'Content-Type': mimetype,
-        'Accept': mimetype
-    }
-    data = {"analytical_data": "[[0.188, 0.192, 0.203], [0.349, 0.346, 0.348]]", "concentration_data": "[[0.008, 0.008, 0.008], [0.016, 0.016, 0.016]]"}
-
-    r = client.post(url + '/linearity_result', data=json.dumps(data), headers=headers)
-    assert r.status_code == 200
+        response = client.post(url + '/linearity_result', data=json.dumps(data), headers=headers)
+        assert response.status_code == 201
