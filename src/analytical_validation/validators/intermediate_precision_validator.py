dfa313bc642d571e8e9480c1a1857cf048791ba0
Alexandre Xavier <ale.bxsantos@gmail.com>
2020-08-15 21:44:51 -0300

    Fixed structure of intermediate precision class

--- src/analytical_validation/validators/intermediate_precision_validator.py
+++ src/analytical_validation/validators/intermediate_precision_validator.py
@@ -7,13 +7,15 @@ import statsmodels.api as statsmodels
 class IntermediatePrecision(object):
     """
     Example:
-        >>> analytical_data = {"day1": [[0.1,0.2,0.1],[0.3,0.3,0.32],[0.41,0.43,0.45],[0.51,0.53,0.55]], "day2": [[0.1,0.2,0.1],[0.3,0.3,0.32],[0.41,0.43,0.45],[0.51,0.53,0.55]], "analyst_a": [[0.1,0.2,0.1],[0.3,0.3,0.32],[0.41,0.43,0.45],[0.51,0.53,0.55]], "analyst_b": [[0.1,0.2,0.1],[0.3,0.3,0.32],[0.41,0.43,0.45],[0.51,0.53,0.55]],}
-        >>> concentration_data = {"day1":[[0.01,0.02,0.01],[0.03,0.03,0.032],[0.041,0.043,0.045],[0.051,0.053,0.055]], "day2":[[0.01,0.02,0.01],[0.03,0.03,0.032],[0.041,0.043,0.045],[0.051,0.053,0.055]], "analyst_a":[[0.01,0.02,0.01],[0.03,0.03,0.032],[0.041,0.043,0.045],[0.051,0.053,0.055]], "analyst_b":[[0.01,0.02,0.01],[0.03,0.03,0.032],[0.041,0.043,0.045],[0.051,0.053,0.055]]}
-        >>> intermediate_precision = IntermediatePrecision(analytical_data, concentration_data)
+        >>> analytical_data = [0.1, 0.11, 0.12, 0.1, 0.11, 0.12, 0.1, 0.11, 0.12, 0.1, 0.11, 0.12,]
+        >>> intercept = 0.0001
+        >>> slope = 20.2
+        >>> alpha = 0.05
+        >>> intermediate_precision = IntermediatePrecision(analytical_data, intercept, slope, alpha)
         >>> intermediate_precision_is_valid = intermediate_precision.validate_intermediate_precision
     """
 
-    def __init__(self, analytical_data, concentration_data, intercept, slope, alpha=0.05):
+    def __init__(self, analytical_data, intercept, slope, alpha=0.05):
         """
         The intermediate precision is the proximity between the results obtained in an analysis of the same sample, in
         the same laboratory, in at least two different days and between two different analysts.
@@ -21,14 +23,15 @@ class IntermediatePrecision(object):
         This class is used to validate the precision of an analytical method given the analytical and concentration data
         of different days and analysts ordered inside a dictionary containing the data inside a list of lists.
         :param analytical_data:
-        :type analytical_data: dict
-        :param concentration_data:
-        :type concentration_data: dict
+        :type analytical_data: list
+        :param intercept:
+        :type intercept: float
+        :param slope:
+        :type slope: float
         :param alpha:
         :type alpha: float
         """
         self.original_analytical_data = analytical_data
-        self.original_concentration_data = concentration_data
         self.intercept = intercept
         self.slope = slope
         self.alpha = alpha
